51d9a34383fb40f546d55b6e91fb3af4d7210d90
Arvind <Arvind@arvinds-mbp.dhcp.wustl.edu>
2017-11-15 14:27:32 -0600

    lab9

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -1,6 +1,14 @@
 package rabinkarp;
 
+import java.util.LinkedList;
+
 public class RK {
+	
+	private int windowLength = 0;
+	private LinkedList<Character> window;
+	private int exp = 1;
+	private int hash = 0;
+	
 	
 	//
 	// Be sure to look at the write up for this assignment
@@ -14,6 +22,14 @@ public class RK {
 	 * @param m size of the window
 	 */
 	public RK(int m) {
+		
+		this.windowLength = m;
+		this.window = new LinkedList<Character>();
+		Character charZero = (char)0;
+		for(int i=0; i<m; i++) {
+			exp = (exp * 31) % 511;
+			window.add(charZero);
+		}
 
 	}
 	
@@ -24,7 +40,20 @@ public class RK {
 	 * @return
 	 */
 	public int nextCh(char d) {
-		return 0;
+		
+		Character deletedCh = (char)0;
+		window.addLast(d);
+		if(window.size() > windowLength) {
+			deletedCh = window.removeFirst();
+		}
+		
+		hash = (hash * 31 - exp * (int)deletedCh + d) % 511;
+		
+		if(hash < 0) {
+			hash += 511;
+		}
+		
+		return hash;
 	}
 
 }
